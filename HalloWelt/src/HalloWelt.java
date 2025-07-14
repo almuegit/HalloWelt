ff2a3d8bc9d44ea2f8f5a7f635824c820fcf425f
blade <blade@rx8>
2025-07-14 19:33:27 +0200

    Initial commit

---
+++ HalloWelt/src/HalloWelt.java
@@ -0,0 +1,31 @@
+/****************************************************
+*
+*	Project :	
+*
+*	Program :	HalloWelt.java
+*
+*	Version	:	TODO
+*
+*	Coder   :	Alexander Müller
+*
+*	Date	:	14.07.2025
+*
+*	Comment	:	TODO
+*			
+*-------------------MODIFICATION-------------------
+*	Coder	:	
+*	Date	:
+*	Comment	:		
+*-------------------MODIFICATION-------------------
+****************************************************/
+
+public class HalloWelt
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("HalloGitWelt");
+
+	}
+
+}
